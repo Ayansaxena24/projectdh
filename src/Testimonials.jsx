a2ec54ae4e87ac0322d9dffe55052f18e3b99a0a
Ayansaxena24 <ayan.saxena24@gmail.com>
2023-03-14 03:45:54 +0530

    making two items visible in Carousel

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -1,97 +1,149 @@
 import React from "react";
 import background from "./background.png";
 import Carousel from "react-material-ui-carousel";
-import { Paper, Button } from '@mui/material';
+import { Paper, Button } from "@mui/material";
 import { ImQuotesLeft } from "react-icons/im";
 
-
 const Testimonials = () => {
-    const navButtonWrapperProps = {
-        buttonWrapper: {
-          position: "absolute",
-          height: "100%",
-          backgroundColor: "transparent",
-          top: 0,
-        },
-        button: {
-          margin: "0 20px", // adjust horizontal position of buttons
-          position: "absolute",
-          top: "50%", // adjust vertical position of buttons
-          transform: "translateY(-50%)",
-          backgroundColor: "white",
-          color: "black",
-          fontSize: "10px",
-          transition: "200ms",
-          cursor: "pointer",
-          "&:hover": {
-            opacity: "0.6 !important",
-          },
-        },
-        // Applies to the "next" button wrapper
-        next: {
-          right: 0,
-          top: 0, // move the button up to align with the top of the carousel
-        },
-        // Applies to the "prev" button wrapper
-        prev: {
-          left: 0,
-          top: 0, // move the button up to align with the top of the carousel
-        },
-      };
-      
+  const navButtonWrapperProps = {
+    buttonWrapper: {
+      position: "absolute",
+      height: "100%",
+      backgroundColor: "transparent",
+      top: 0,
+    },
+    button: {
+      margin: "0 20px", // adjust horizontal position of buttons
+      position: "absolute",
+      top: "50%", // adjust vertical position of buttons
+      transform: "translateY(-50%)",
+      backgroundColor: "white",
+      color: "black",
+      fontSize: "10px",
+      transition: "200ms",
+      cursor: "pointer",
+      "&:hover": {
+        opacity: "0.6 !important",
+      },
+    },
+    // Applies to the "next" button wrapper
+    next: {
+      right: 0,
+      top: 0, // move the button up to align with the top of the carousel
+    },
+    // Applies to the "prev" button wrapper
+    prev: {
+      left: 0,
+      top: 0, // move the button up to align with the top of the carousel
+    },
+  };
 
+  const carouselStyle = {
+    backgroundColor: "#F0F0F0", // change background color of the carousel
+  };
+  const items = [
     {
-        var items = [
-            {
-                name: "Neha Mishra",
-                description: "Decent architecture designs. Appreciate their professional approach towards work."
-            },
-            {
-                name: "Dubash Pal",
-                description: "Highly recommended this firm to one and all. Their architectural team is extremely professional."
-            },
-            {
-                name: "Pankaj Sharma",
-                description: "Great architects found here. They understand the needs of the client."
-            }
-        ]
-    }
+      name: "Neha Mishra",
+      description:
+        "Decent architecture designs. Appreciate their professional approach towards work.",
+    },
+    {
+      name: "Dubash Pal",
+      description:
+        "Highly recommended this firm to one and all. Their architectural team is extremely professional.",
+    },
+    {
+      name: "Pankaj Sharma",
+      description:
+        "Great architects found here. They understand the needs of the client.",
+    },
+  ];
 
-    return (
-        <div className="flex flex-col sm:flex-row justify-center items-center">
-            <img src={background} className="h-50 w-3/4 sm:w-1/2 relative"/> 
-            <Carousel className="w-full absolute right-0"
-            animation="slide"
-            stopAutoPlayOnHover="true"
-            duration={300}
-            interval={4000}
-            fullHeightHover={true}
-            navButtonsAlwaysVisible={true}
-            navButtonWrapperProps={navButtonWrapperProps}
-            >
-                {
-                    items.map((item,i)=> <Item key={i} item={item} navButtonWrapperProps={navButtonWrapperProps}/>)
-                }
-            </Carousel>
-        </div>
+// // Add duplicates of the first two items at the end of the array
+// items.push(items[0], items[1]);
 
-
+return (
+    <div className="flex flex-col sm:flex-row justify-center items-center">
+    <img src={background} className="h-50 w-3/4 sm:w-1/2 relative" />
+    <Carousel
+    className="w-full absolute right-20"
+    style={carouselStyle}
+    animation="slide"
+    stopAutoPlayOnHover="true"
+    duration={300}
+    slides={3}
+    interval={4000}
+    fullHeightHover={false}
+    navButtonsAlwaysVisible={true}
+    visibleSlides={2}
+    navButtonWrapperProps={navButtonWrapperProps}
+    navButtonsProps={{
+    // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+    style: {
+    backgroundColor: "black",
+    borderRadius: 20,
+    },
+    }}
+    navButtonsWrapperProps={{
+    // Move the buttons to the bottom. Unsetting top here to override default style.
+    style: {
+    bottom: "36px",
+    margin: "0px",
+    top: "unset",
+    },
+    }}
+    >
+    {items.map((item, i) => {
+          // check if there are two more items to display
+          if (i + 2 <= items.length) {
+            return (
+              <TwoItems
+                key={i}
+                item={items[i]}
+                item2={items[i + 1]}
+                navButtonWrapperProps={navButtonWrapperProps}
+              />
+            );
+          }
+          // if there is only one item left to display
+          return (
+            <TwoItems
+              key={i}
+              item={items[i]}
+              item2={items[0]}
+              navButtonWrapperProps={navButtonWrapperProps}
+            />
+          );
+        })}
+      </Carousel>
+    </div>
     );
-}
-
-function Item(props)
-{
+    };
+    
+    function TwoItems(props) {
+      return (
+        <div className="flex gap-2 scale-75 lg:scale-100 bg-transparent px-8">
+          <Item item={props.item} navButtonWrapperProps={props.navButtonWrapperProps} />
+          <Item item={props.item2} navButtonWrapperProps={props.navButtonWrapperProps} />
+        </div>
+      );
+    }
+    
+    function Item(props) {
     return (
-        <Paper className="flex flex-col gap-4 text-sm py-2 bg-transparent py-6" navButtonWrapperProps={props.navButtonWrapperProps}>
-            <div className="flex flex-row gap-3">
-              <div className="rounded-full p-3 bg-orange-400 relative left-2 scale-75">
-                {<ImQuotesLeft className="absolute bottom-3 right-1 text-xl" />}
-              </div>
-              <h2 className="font-bold left-2">{props.item.name}</h2>
-            </div>
-            <p className="flex flex-wrap text-start pl-3">{props.item.description}</p>
-        </Paper>
-    )
-}
+    <Paper
+       className="flex flex-col gap-4 text-sm py-2 bg-transparent py-4 px-4"
+       navButtonWrapperProps={props.navButtonWrapperProps}
+     >
+    <div className="flex flex-row gap-3">
+    <div className="rounded-full p-3 bg-orange-400 relative left-2 scale-75">
+    {<ImQuotesLeft className="absolute bottom-3 right-1 text-xl" />}
+    </div>
+    <h2 className="font-bold left-2">{props.item.name}</h2>
+    </div>
+    <p className="flex flex-wrap text-start pl-3">{props.item.description}</p>
+    </Paper>
+    );
+    }
 
 export default Testimonials;
--- src/Videos.jsx
+++ src/Videos.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 
-// Render a YouTube video player
 const Videos = () => {
     return (
-        <div className='rounded-sm border-2 border-gray-100 p-2 justify-center items-center pb-12'>
+        <div className='rounded-sm border-2 border-gray-100 p-2 justify-center items-center pb-16'>
             <h1 className="font-bold text-3xl text-center py-12">Videos</h1>
-            <div className='flex flex-row gap-4 w-full justify-center items-center'>
-            <div className='flex flex-col gap-4'>
-               <ReactPlayer 
-               url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-               playing='true'
-               controls='true'
-            //    width={400}
-            //    light='true'
-                             />
-               <p className='text-center font-semibold text-lg'>Why hire an Architect</p>
-            </div>
-            <div className='flex flex-col gap-4'>
-               <ReactPlayer 
-               url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-               playing='true'
-               controls='true'
-            //    width={400}
-            //    light='true'
-               style={{borderradius:25}}
-                             />
-               <p className='text-center font-semibold text-lg'>What does an Architect do?</p>
-            </div>
-               
+            <div className='flex flex-col sm:flex-row gap-6 w-full justify-center items-center'>
+                <div className='flex flex-col gap-4 rounded-md overflow-hidden'>
+                    <div className='relative py-36 px-32 md:py-36 md:px-40 lg:px-56 lg:py-52 xl:px-64 xl:py-52'>
+                     <div className='rounded-3xl'>
+                        <ReactPlayer
+                            url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                            playing='true'
+                            controls='true'
+                            width='100%'
+                            height='100%'
+                            style={{ position: 'absolute', top: 0, left: 0 }}
+                        />
+                        </div>
+                    </div>
+                    <p className='text-center font-semibold text-lg'>Why hire an Architect</p>
+                </div>
+                <div className='flex flex-col gap-4 rounded-md overflow-hidden'>
+                    <div className='relative py-36 px-32 md:py-36 md:px-40 lg:px-56 lg:py-52 xl:px-64 xl:py-52'>
+                        <ReactPlayer
+                            url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                            playing='true'
+                            controls='true'
+                            width='100%'
+                            height='100%'
+                            style={{ position: 'absolute', top: 0, left: 0 }}
+                        />
+                    </div>
+                    <p className='text-center font-semibold text-lg'>What does an Architect do?</p>
+                </div>
             </div>
         </div>
     )
 }
+
 export default Videos
